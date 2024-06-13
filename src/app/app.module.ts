9801942ae9a8d98ea829b95292edae9f4547e85e
Fabrizio Mayor <klatort@gmail.com>
2024-06-13 16:37:37 -0500

    uffff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LadingPageComponent } from './lading-page/lading-page.component';
@@ -46,6 +45,11 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    MatInputModule, 
+    MatFormFieldModule, 
+    BrowserAnimationsModule,
     HttpClientModule,
     MatButtonModule,
     MatIconModule,
