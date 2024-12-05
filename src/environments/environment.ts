f850e087bcc1789beee1248e444a07a7fb699bde
klatort <33676805+klatort@users.noreply.github.com>
2024-12-05 10:47:27 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: 'http://176.52.133.163:3000',
+    apiUrl: 'https://176.52.133.163:3000',
     apiAccessKey: '1a989ff5-3c6f-405d-bef3-66f326a700d8',
     apiSecretKey: '4183d3bf50c85ee6dee3d989c667b444'
 };
