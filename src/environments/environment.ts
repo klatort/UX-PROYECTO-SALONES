8c5fb2e9b4cf1ca92cd5e3a544f79a37e821c1e7
Fabrizio Mayor <33676805+klatort@users.noreply.github.com>
2025-07-05 01:34:45 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     apiUrl: 'https://salones.ddns.net',
-    apiAccessKey: '1a989ff5-3c6f-405d-bef3-66f326a700d8',
-    apiSecretKey: '4183d3bf50c85ee6dee3d989c667b444'
+    apiAccessKey: '99e85b0a-7b92-4e15-b722-0712c1176df5',
+    apiSecretKey: 'f18b89547a22f6c9a7087ce412d434f0'
 };
